3d008368e3f98d2d850c7e62689950790f0ecca2
James James <jamesjay4199@gmail.com>
2020-12-06 21:25:04 +0100

    Number input fixes

--- src/components/rentals.vue
+++ src/components/rentals.vue
@@ -16,19 +16,29 @@
           type="primary"
           >Add New</el-button
         >
-        <el-dialog title="New Rental" :visible.sync="newRentalDialog" :close-on-click-modal="false" :close-on-press-escape="false">
+        <el-dialog
+          title="New Rental"
+          :visible.sync="newRentalDialog"
+          :close-on-click-modal="false"
+          :close-on-press-escape="false"
+        >
           <el-upload
-              class="upload-demo"
-              :auto-upload="false"
-              :on-remove="handleRemove"
-              :file-list="imageList"
-              drag
-              action="https://jsonplaceholder.typicode.com/posts/"
-              multiple
-              :limit="3">
-              <i class="el-icon-upload"></i>
-              <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-              <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
+            class="upload-demo"
+            :auto-upload="false"
+            :on-remove="handleRemove"
+            :file-list="imageList"
+            drag
+            action="https://jsonplaceholder.typicode.com/posts/"
+            multiple
+            :limit="3"
+          >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              Drop file here or <em>click to upload</em>
+            </div>
+            <div class="el-upload__tip" slot="tip">
+              jpg/png files with a size less than 500kb
+            </div>
           </el-upload>
           <el-form
             :rules="rules"
@@ -36,7 +46,7 @@
             ref="newRentalForm"
             status-icon
             :label-position="'top'"
-            >
+          >
             <el-form-item label="Name" prop="name">
               <el-input
                 v-model="newRentalForm.name"
@@ -47,12 +57,14 @@
               <div class="col-md-6">
                 <el-form-item label="Price" prop="price">
                   <el-input
+                    type="text"
                     class="price_input"
-                    @input="($event) => {this.formattedPrice = $event }"
+                    @input="handlePriceInput"
+                    :value="formattedPrice"
                     autocomplete="off"
                   ></el-input>
                 </el-form-item>
-                {{formattedPrice}}
+                {{ formattedPrice }}
               </div>
               <div class="col-md-6">
                 <el-form-item label="Quantity" prop="quantity">
@@ -61,7 +73,7 @@
                     v-model.number="newRentalForm.quantity"
                   ></el-input>
                 </el-form-item>
-                {{formattedQuantity}}
+                {{ formattedQuantity }}
               </div>
             </div>
             <el-form-item label="Description" prop="description">
@@ -73,7 +85,9 @@
             </el-form-item>
             <div class="d-flex justify-content-between">
               <el-button @click="cancelForm('newRentalForm')">Cancel</el-button>
-              <el-button type="primary" @click="submitForm('newRentalForm')">Create</el-button>
+              <el-button type="primary" @click="submitForm('newRentalForm')"
+                >Create</el-button
+              >
             </div>
           </el-form>
         </el-dialog>
@@ -83,7 +97,7 @@
       ref="multipleTable"
       :data="
         tableData.filter(
-          (data) =>
+          data =>
             !search || data.name.toLowerCase().includes(search.toLowerCase())
         )
       "
@@ -121,8 +135,8 @@ export default {
           name: "Microphone 2e45",
           created: "11-22-3333",
           cost: "$50",
-          status: "Cancelled",
-        },
+          status: "Cancelled"
+        }
       ],
       multipleSelection: [],
       search: "",
@@ -130,40 +144,57 @@ export default {
       imageList: [],
       newRentalForm: {
         name: "",
-        price: null,
+        price: "",
         quantity: 1,
         description: "",
-        image: "",
+        image: ""
       },
       rules: {
         name: [
           {
             required: true,
             message: "product name is required",
-            trigger: ["blur", "change"],
+            trigger: ["blur", "change"]
           },
-          { min: 3, message: "name is too short", trigger: ["blur", "change"] },
+          { min: 3, message: "name is too short", trigger: ["blur", "change"] }
         ],
         price: [
-          { required: true, message: "price is required", trigger: ["blur", "change"] },
-          { type: 'number', message: 'quantity must be a number'}
+          {
+            required: true,
+            message: "price is required",
+            trigger: ["blur", "change"]
+          },
+          { type: "number", message: "quantity must be a number" }
         ],
         quantity: [
-          { required: true, message: "quantity is required", trigger: ["blur", "change"] },
-          { type: 'number', message: 'quantity must be a number'}
+          {
+            required: true,
+            message: "quantity is required",
+            trigger: ["blur", "change"]
+          },
+          { type: "number", message: "quantity must be a number" }
         ],
         description: [
           {
             required: true,
             message: "product description is required",
-            trigger: ["blur", "change"],
+            trigger: ["blur", "change"]
           },
-          { min: 3, message: "description is too short", trigger: ["blur", "change"] },
-        ],
-      },
+          {
+            min: 3,
+            message: "description is too short",
+            trigger: ["blur", "change"]
+          }
+        ]
+      }
     };
   },
   methods: {
+    handlePriceInput(price) {
+      this.$nextTick().then(() => {
+        this.formattedPrice = price;
+      });
+    },
     handleEdit(index, row) {
       console.log(index, row);
     },
@@ -179,69 +210,79 @@ export default {
         lock: true,
         text: "Loading",
         spinner: "el-icon-loading",
-        background: "rgba(0, 0, 0, 0.7)",
+        background: "rgba(0, 0, 0, 0.7)"
       });
       setTimeout(() => {
         loading.close();
       }, 2000);
     },
     submitForm(rentalsForm) {
-      this.$refs[rentalsForm].validate((valid) => {
+      this.$refs[rentalsForm].validate(valid => {
         if (valid) {
           this.$message({
             message: "Product created",
-            type: "success",
+            type: "success"
           });
-          Object.keys(this.newRentalForm).map((item) => {
-            if(typeof(this.newRentalForm[item]) == "string"){
-                this.newRentalForm[item] = this.newRentalForm[item].trim();
+          Object.keys(this.newRentalForm).map(item => {
+            if (typeof this.newRentalForm[item] == "string") {
+              this.newRentalForm[item] = this.newRentalForm[item].trim();
             }
-          })
-          console.log(this.newRentalForm)
+          });
+          console.log(this.newRentalForm);
         } else {
           this.$message.error("Oops, Something is not right");
           return false;
         }
       });
     },
-    async cancelForm(rentalsForm){
-          this.$confirm(`All information will be lost. Are you sure?`).then(()=> {
-              this.newRentalDialog = false
-              this.$refs[rentalsForm].resetFields();
-              this.$message({
-                  type: 'info',
-                  message: 'Create Rental cancelled'
-                }); 
-          }).catch(()=> {});
+    async cancelForm(rentalsForm) {
+      this.$confirm(`All information will be lost. Are you sure?`)
+        .then(() => {
+          this.newRentalDialog = false;
+          this.$refs[rentalsForm].resetFields();
+          this.$message({
+            type: "info",
+            message: "Create Rental cancelled"
+          });
+        })
+        .catch(() => {});
     },
-    handleRemove(image, imageList){
-      console.log(this.imageList, image, imageList)
+    handleRemove(image, imageList) {
+      console.log(this.imageList, image, imageList);
     }
-    
   },
+
   computed: {
     formattedPrice: {
-    // getter
-    get: function () {
-      return new Intl.NumberFormat('en-NG', { style: 'currency', currency: 'NGN' }).format(this.newRentalForm.price);
+      get() {
+        return new Intl.NumberFormat("en-NG", {
+          style: "currency",
+          currency: "NGN",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0
+        }).format(this.newRentalForm.price);
+      },
+      set(value) {
+        value = value.replace(/₦/g, "");
+        value = value.replace(/,/g, "");
+
+        if (Number.isNaN(Number(value))) return;
+        this.newRentalForm.price = Number(value);
+      }
     },
-    // setter
-    set: function (newValue) {
-      this.newRentalForm.price = newValue
-    }
-  },
     formattedQuantity: function() {
       return new Intl.NumberFormat().format(this.newRentalForm.quantity);
     }
-  },
+  }
 };
 </script>
 
-<style  lang="scss">
-.el-dialog{
+<style lang="scss">
+.el-dialog {
   width: fit-content !important;
 }
-.el-upload-dragger, .el-upload{
+.el-upload-dragger,
+.el-upload {
   width: 100% !important;
 }
 </style>
